20009718d0a6fc00f0ad62cfa29d3ea98283aeeb
Donmark2k <chukwuemekavp@gmail.com>
2023-04-04 11:10:15 +0100

    Add explain analyze in queries.sql

--- queries.sql
+++ queries.sql
@@ -95,3 +95,6 @@ SELECT * FROM visits where vet_id = 2;
 SELECT * FROM owners where email = 'owner_18327@mail.com';
 
 
+EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animal_id = 4;
+EXPLAIN ANALYZE SELECT * FROM visits where vet_id = 2;
+EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
